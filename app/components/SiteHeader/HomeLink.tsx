e5e2c3511bf890fa5b3609b959de5d571cea5502
Lukas Cornille <lukas.cornille@inthepocket.com>
2025-01-23 11:36:35 +0100

    fix: update home link text size

--- app/components/SiteHeader/HomeLink.tsx
+++ app/components/SiteHeader/HomeLink.tsx
@@ -9,7 +9,7 @@ export const HomeLink = () => {
       prefetch="intent"
       aria-label="Home"
       className={cn(
-        'flex h-8 w-8 items-center justify-center rounded bg-text p-1 text-base no-underline',
+        'flex h-8 w-8 items-center justify-center rounded bg-text p-1 text-sm no-underline',
         'focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-text',
       )}
     >
